2bf97ddc61b5f99cde7ec7e3aebff10579c6b462
montse110666 <73588076+montse110666@users.noreply.github.com>
2022-04-23 13:04:14 +0200

    Add files via upload

---
+++ Tarea práctica UT 8/src/ejercicio1/PrimaMainMetodos.java
@@ -0,0 +1,130 @@
+package ejercicio1;
+import java.util.Scanner;
+/**
+ * Clase PrimaMainMetodos
+ * Esta clase calcula la prima de un empleado, dependiendo
+ * de si es o no directivo y del número de meses que ha trabajado.
+ * @author Montse
+ * @version 2020-09 (4.17.0)
+ * @date 2022/04/22
+ */
+
+
+public class PrimaMainMetodos {
+	
+
+	static Scanner  teclado=new Scanner(System.in);
+	
+	/**
+	 * Este método se encarga de iniciar la ejecución del programa
+	 * Éste es el método principal del proyecto
+	 * @param args[] es un arreglo con los parámetros que él reciba por consola
+	 * 
+	*/
+
+	public static void main(String[] args) {
+
+		int numEmple;
+		String nomEmple;
+		int meses;
+		char esDirectivo;
+		char respuesta;
+		
+
+		do {
+			System.out.println("\nDATOS  EMPLEADO/A");
+			numEmple=leerNumEmple();
+			nomEmple=leerNomEmple();
+			meses=leerMeses();
+			esDirectivo=leerEsDirectivo();
+			System.out.println("\n\tLe corresponde la prima "+hallarPrima(esDirectivo, meses));
+			
+			
+			System.out.println("\n¿CALCULAR MAS PRIMAS? (S/N): ");
+			respuesta=teclado.nextLine().toUpperCase().charAt(0);
+		}while(respuesta=='S');		
+	}
+/**
+ * Este método se encarga de calcular la prima
+ * que le corresponde al empleado. Si es directivo, si los meses
+ * son iguales o mayores a 12, devolverá P1 y sino, devolverá
+ * P3. Si no es directivo, si los meses son iguales o mayores a 12
+ * devolverá P2 y sino devolverá P4.
+ * @param esDirectivo es un parámetro de tipo carácter, que indica 
+ * si el empleado es o no directivo.
+ * meses es un parámetro de tipo entero, que indica el número de
+ * meses
+ * @return El tipo de prima que le corresponde al empleado
+ */
+
+	public static String hallarPrima(char esDirectivo, int meses) {
+		if(esDirectivo=='+') // ES DIRECTIVO
+			if(meses>=12)
+				return "P1";
+			else
+				return "P3";
+		else 	// NO ES DIRECTIVO
+			if(meses>=12)
+				return "P2";
+			else
+				return "P4";
+	}
+
+	/**
+	 * Este método se encarga de leer por teclado el número de empleado,
+	 * que tiene que estar entre 100 y 999.
+	 * @return El número de empleado
+	 */
+	public static int leerNumEmple() {		
+		int numEmple;
+		do{
+			System.out.println("NÚMERO [100-999]: ");
+			numEmple = teclado.nextInt();
+		}while(numEmple<100 || numEmple>999);		
+		teclado.nextLine();
+		return numEmple;
+	}
+	
+	/**
+	 * Este método se encarga de leer por teclado el nombre del empleado,
+	 * que tiene que tener máximo 10 caracteres.
+	 * @return El nombre del empleado
+	 */
+	public static String leerNomEmple() {
+		String nomEmple;
+		do {
+			System.out.println("NOMBRE (max 10 caracteres): ");
+			nomEmple = teclado.nextLine();
+		}while(nomEmple.length()>10);		
+		return nomEmple;
+	}
+	
+	/**
+	 * Este método se encarga de leer por teclado los meses de trabajo del empleado,
+	 * que no pueden ser menores que 0.
+	 * @return el número de meses trabajados por el empleado.
+	 */
+	public static int leerMeses() {
+		int meses;
+		do {
+			System.out.println("MESES DE TRABAJO: ");
+			meses = teclado.nextInt();
+		}while(meses<0);
+		teclado.nextLine();
+		return meses;
+	}
+	
+	/**
+	 * Este método se encarga de leer por teclado si el empleado es directivo (+) o no (-)-
+	 * @return un + (el empleado es directivo) o un - (el empleado no es directivo).
+	 */
+
+	public static char leerEsDirectivo() {
+		char esDirectivo;
+		do {
+			System.out.println("¿ES DIRECTIVO? (+/-): ");
+			esDirectivo = teclado.nextLine().charAt(0);
+		}while(esDirectivo!='+' && esDirectivo!='-');
+		return esDirectivo;
+	}
+}
